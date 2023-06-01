54b5e3d1e6453b2c85cc91f159d58d3d9826e3e0
admin <admin@DESKTOP-O4V9ADF>
2023-06-01 17:52:26 +0900

    a

--- HelloApp/src/comm/yedam/object/Bank.java
+++ HelloApp/src/comm/yedam/object/Bank.java
@@ -1,5 +1,8 @@
 package comm.yedam.object;
 
 public class Bank {
-
+	String accountNo;//unique.
+	String owner;//계좌주
+	int balance;//잔고
+	
 }
--- HelloApp/src/comm/yedam/object/ObjectEx2Bank.java
+++ HelloApp/src/comm/yedam/object/ObjectEx2Bank.java
@@ -1,5 +1,38 @@
 package comm.yedam.object;
 
-public class ObjectEx2Bank {
+import java.util.Scanner;
 
+public class ObjectEx2Bank {
+	public static void main(String[] args) {
+		Bank[] accounts = new Bank[3];
+
+		boolean run = true;
+		int accountNo=0;
+		Scanner scn = new Scanner(System.in);
+
+		while (run) {
+			System.out.println("1.등록 2.조회 3.입금 4.출금 5.종료");
+			System.out.print("선택> ");
+			int selectNo = Integer.parseInt(scn.nextLine());
+
+			if (selectNo == 1) {// 등록
+				System.out.print("계좌를 등록해주세요:");
+				 accountNo=Integer.parseInt(scn.nextLine());
+				 accounts = new Bank[accountNo];
+			} // 조회
+			else if (selectNo == 2) {
+				for (int i = 0; i < accounts.length; i++) {
+					System.out.print("acc[" + i + "]");
+					[i] = Integer.parseInt(scn.nextLine());
+			} // 입금
+			else if (selectNo == 3) {
+				//출금
+			} else if (selectNo == 4) {
+				//종료
+			} else if (selectNo == 5) {
+				run=false;
+				System.out.println("프로그램종료");
+			}
+		}
+	}
 }
